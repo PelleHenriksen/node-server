4d0e5da7b4f09e1c677aea573e373032a1f8b856
Pelle Henriksen <116062364+Pelle980@users.noreply.github.com>
2023-11-20 09:06:06 +0100

    Add user creation, deletion, and update functionality to the server.js file.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+import server from "./lib/server.js";
+
 const app = {};
 
-app.init = () => {};
+app.init = () => {
+  const port = 3000;
 
-/* Starter app */
+  server.listen(3000, () => {
+    console.log("\n------ SERVER ------ \n");
+    console.log("http://localhost:" + port);
+    console.log("\n------ ****** ------ \n");
+  });
+};
+
+// Starter Applikation
 app.init();
